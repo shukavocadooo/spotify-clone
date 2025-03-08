342287a91f3f155e70a4f876bf1096ad67b1fed6
avocado <shukriramli010214@gmail.com>
2025-03-08 09:55:03 +0800

    Add new song and new album

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -32,6 +32,7 @@ import img7 from './img7.jpg'
 import img8 from './img8.jpg'
 import img9 from './img9.jpg'
 import img10 from './img10.jpg'
+import avocado from './avocado.jpg'
 import img11 from './img11.jpg'
 import img12 from './img12.jpg'
 import img13 from './img13.jpg'
@@ -41,6 +42,20 @@ import img16 from './img16.jpg'
 import song1 from  './song1.mp3'
 import song2 from  './song2.mp3'
 import song3 from  './song3.mp3'
+import song4 from './song4.mp3'
+import song5 from './song5.mp3'
+import song6 from './song6.mp3'
+import song7 from './song7.mp3'
+import song8 from './song4.mp3'
+import song9 from './song9.mp3'
+import song10 from './song10.mp3'
+import song11 from './song11.mp3'
+import song12 from './song12.mp3'
+import song13 from './song13.mp3'
+import song14 from './song14.mp3'
+import song15 from './song15.mp3'
+import song16 from './song16.mp3'
+import song17 from './song17.mp3'
 
 export const assets = {
     bell_icon,
@@ -72,110 +87,162 @@ export const assets = {
 export const albumsData = [
     {   
         id:0,
-        name: "Top 50 Global",
-        image: img8,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#2a4365"
+        name: "Avocado",
+        image: avocado,
+        desc:"Wicked is good",
+        bgColor:"#2a4365",
+        songIds: [0,1,2,4,5,6,7,8,9,10,11,12,13,14]
     },
     {   
         id:1,
-        name: "Top 50 Malaysia",
-        image: img9,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#22543d"
-    },
-    {   
-        id:2,
-        name: "Trending Malaysia",
-        image: img10,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#742a2a"
-    },
-    {   
-        id:3,
-        name: "Trending Global",
-        image: img16,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#44337a"
-    },
-    {   
-        id:4,
-        name: "Mega Hits,",
-        image: img11,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#234e52"
-    },
-    {   
-        id:5,
-        name: "Happy Favorites",
-        image: img15,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#744210"
+        name: "Markisa",
+        image: img2,
+        desc:"-_-",
+        bgColor:"#2a4365",
+        songIds: [15,16]
     }
 ]
 
 export const songsData = [
     {
         id:0,
-        name: "Song One",
-        image: img1,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
+        name: "Beutiful Things",
+        image: avocado,
+        file: song1,
         duration:"3:00"
     },
     {
         id:1,
-        name: "Song Two",
-        image: img2,
+        name: "Heat Waves",
+        image: avocado,
         file:song2,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:20"
     },
     {
         id:2,
-        name: "Song Three",
-        image: img3,
+        name: "One Right Now",
+        image: avocado,
         file:song3,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:32"
     },
     {
         id:3,
-        name: "Song Four",
-        image: img4,
-        file:song1,
+        name: "Candy Paint",
+        image: avocado,
+        file:song4,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:50"
     },
     {
         id:4,
-        name: "Song Five",
-        image: img5,
-        file:song2,
+        name: "Go Flex",
+        image: avocado,
+        file:song5,
         desc:"Put a smile on your face with these happy tunes",
         duration:"3:10"
     },
     {
         id:5,
-        name: "Song Six",
-        image: img14    ,
-        file:song3,
+        name: "Goodbyes (feat. Young Thug)",
+        image: avocado,
+        file:song6,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:45"
     },
     {
         id:6,
-        name: "Song Seven",
-        image: img7,
-        file:song1,
+        name: "Hate Me",
+        image: avocado,
+        file:song7,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:18"
     },
     {
         id:7,
-        name: "Song Eight",
-        image: img12,
-        file:song2,
+        name: "Hollywood's Bleeding",
+        image: avocado,
+        file:song8,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    },
+    {
+        id:8,
+        name: "Hide (feat. Seexyn)",
+        image: avocado,
+        file:song9,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    },
+    {
+        id:9,
+        name: "Intentions (feat. Quavo)",
+        image: avocado,
+        file:song10,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:10,
+        name: "Psycho (feat.Ty Dolla $ign)",
+        image: avocado,
+        file:song11,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:11,
+        name: "Better Now",
+        image: avocado,
+        file:song12,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:12,
+        name: "Congratulations (feat. Quavo)",
+        image: avocado,
+        file:song13,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:13,
+        name: "Sunflower - Spider-Man_Into the Spider-Verse",
+        image: avocado,
+        file:song14,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:14,
+        name: "Circles",
+        image: avocado,
+        file:song15,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:15,
+        name: "Kenangan Terindah (Samsons)",
+        image: img2,
+        file:song16,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:35"
+    }
+    ,
+    {
+        id:16,
+        name: "Saat Terakhir",
+        image: img2,
+        file:song17,
         desc:"Put a smile on your face with these happy tunes",
         duration:"2:35"
     }
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -10,17 +10,17 @@ const DisplayHome = () => {
     <>
     <Navbar/>
     <div className='mb-4'>
-        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <h1 className='my-5 font-bold text-2xl'>Whats this</h1>
         <div className='flex overflow-auto'>
             {albumsData.map((item,index)=>(<AlbumItems key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
         </div>
     </div>
-    <div className='mb-4'>
+    {/* <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
         <div className='flex overflow-auto'>
             {songsData.map((item,index)=>(<SongItems key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
         </div>
-    </div>
+    </div> */}
     </>
   )
 }
